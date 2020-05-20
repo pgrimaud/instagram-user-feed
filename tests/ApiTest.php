4097d287903d52ad6bbb0d42108a5caccd9cd967
Pierre Grimaud <grimaud.pierre@gmail.com>
2020-05-20 18:22:16 +0200

    Add tests

---
+++ tests/ApiTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Instagram\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ApiTest extends TestCase
+{
+    public function testEmptyCacheValueOnJsonFeed()
+    {
+        $this->assertSame(true, true);
+    }
+}
