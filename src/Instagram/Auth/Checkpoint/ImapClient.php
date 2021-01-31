488e15cf6be0c598acb05a0801be1cc1b845a0a2
Pierre Grimaud <grimaud.pierre@gmail.com>
2021-01-31 13:53:56 +0100

    Suppress imap_open errors (refs to #184)

--- src/Instagram/Auth/Checkpoint/ImapClient.php
+++ src/Instagram/Auth/Checkpoint/ImapClient.php
@@ -100,10 +100,12 @@ class ImapClient
      */
     public function getLastInstagramEmailContent(int $try = 1): string
     {
-        $resource  = imap_open('{' . $this->getServer() . '/' . $this->getConnectionType() . '/ssl}INBOX', $this->getLogin(), $this->getPassword());
+        $resource  = @imap_open('{' . $this->getServer() . '/' . $this->getConnectionType() . '/ssl}INBOX', $this->getLogin(), $this->getPassword());
+
         if (!$resource) {
           throw new InstagramAuthException('Unable to open IMAP stream.');
         }
+
         $numberMax = imap_num_msg($resource);
 
         $foundCode = false;
