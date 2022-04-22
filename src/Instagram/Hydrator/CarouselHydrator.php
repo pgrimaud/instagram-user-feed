302bd3263dfb92b6512b0ca21178769791623728
nsmle <fikiproductionofficial@gmail.com>
2022-04-22 09:29:52 +0700

    Add method to hydrate carousel type media

---
+++ src/Instagram/Hydrator/CarouselHydrator.php
@@ -0,0 +1,141 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Instagram\Hydrator;
+
+use Instagram\Exception\InstagramFetchException;
+use Instagram\Hydrator\UserInfoHydrator;
+use Instagram\Model\{Media, Carousel};
+use Instagram\Utils\InstagramHelper;
+
+class CarouselHydrator
+{
+    /**
+     * @var UserInfoHydrator
+     */
+    private $hydrateUser;
+
+    public function __construct()
+    {
+        $this->hydrateUser = new UserInfoHydrator();
+    }
+
+    /**
+     * @param \StdClass $node
+     *
+     * @return Carousel
+     */
+    public function carouselBaseHydrator(\StdClass $node): Carousel
+    {
+        $carousel = new Carousel();
+
+        $carousel->setId($node->pk);
+        $carousel->setShortCode($node->code);
+        $carousel->setLink(InstagramHelper::URL_BASE . "p/{$node->code}/");
+        $carousel->setDate(\DateTime::createFromFormat('U', (string) $node->taken_at));
+        $carousel->setLikes($node->like_count);
+        $carousel->setIsLiked($node->has_liked);
+        $carousel->setComments($node->comment_count);
+        $carousel->setHeight($node->carousel_media[0]->original_height);
+        $carousel->setWidth($node->carousel_media[0]->original_width);
+
+        $carouselMedia = $this->carouselMediaHydrator($node->carousel_media);
+        $carousel->setCarousel($carouselMedia);
+
+        if (property_exists($node, 'caption')) {
+            $carousel->setCaption($node->caption->text);
+            $carousel->setHashtags(InstagramHelper::buildHashtags($node->caption->text));
+        }
+
+        if (property_exists($node, 'location')) {
+            $carousel->setLocation($node->location);
+        }
+
+        if (property_exists($node, 'usertags')) {
+            $userTags = [];
+            foreach ($node->usertags->in as $user) {
+                $userTags[] = $this->hydrateUser->userBaseHydrator($user->user);
+            }
+
+            $carousel->setUserTags($userTags);
+        }
+
+        $user = $this->hydrateUser->userBaseHydrator($node->user);
+        $carousel->setUser($user);
+
+        return $carousel;
+    }
+
+    /**
+     * @param array $carouselItems
+     *
+     * @return array
+     */
+    public function carouselMediaHydrator(array $carouselItems): array
+    {
+        $carouselMedias = [];
+        foreach ($carouselItems as $carouselItem) {
+            $carouselType = $this->getTypeCarousel($carouselItem->media_type);
+
+            if ($carouselType == Media::TYPE_IMAGE) {
+                $carouselMedia = [
+                    'id'                   => $carouselItem->pk,
+                    'parentId'     => $carouselItem->carousel_parent_id,
+                    'type'                 => $carouselType,
+                    'width'                => $carouselItem->original_width,
+                    'height'               => $carouselItem->original_height,
+                ];
+
+                if (property_exists($carouselItem, 'image_versions2')) {
+                    $carouselMedia['image'] = $carouselItem->image_versions2->candidates;
+                }
+
+                if (property_exists($carouselItem, 'video_versions')) {
+                    $carouselMedia['video'] = $carouselItem->video_versions;
+                }
+
+                if (property_exists($carouselItem, 'image_versions2')) {
+                    $carouselMedia['accessibilityCaption'] = $carouselItem->accessibility_caption;
+                }
+
+                if (property_exists($carouselItem, 'usertags')) {
+                    $userTags = [];
+                    foreach ($node->usertags->in as $user) {
+                        $userTags[] = $this->hydrateUser->userBaseHydrator($user->user);
+                    }
+
+                    $carouselMedia['userTags'] = $userTags;
+                }
+            }
+
+            $carouselMedias[] = (object) $carouselMedia;
+        }
+
+        return $carouselMedias;
+    }
+
+    /**
+     * @param int $media_type
+     * @return string
+     * @throws InstagramFetchException
+     */
+    private function getTypeCarousel(int $media_type): string
+    {
+        switch ($media_type) {
+            case Media::MEDIA_TYPE_IMAGE:
+                $type = Media::TYPE_IMAGE;
+                break;
+            case Media::MEDIA_TYPE_VIDEO:
+                $type = Media::TYPE_VIDEO;
+                break;
+            case Media::MEDIA_TYPE_CAROUSEL:
+                $type = Media::TYPE_CAROUSEL;
+                break;
+            default:
+                throw new InstagramFetchException('Media type ' . $media_type . ' not found');
+        }
+
+        return $type;
+    }
+}
---
+++ src/Instagram/Model/Carousel.php
@@ -0,0 +1,323 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Instagram\Model;
+
+class Carousel
+{
+    /**
+     * @var string
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $shortCode;
+
+    /**
+     * @var string
+     */
+    private $link;
+
+    /**
+     * @var int
+     */
+    private $likes;
+
+    /**
+     * @var boolean
+     */
+    private $isLiked = false;
+
+    /**
+     * @var int
+     */
+    private $comments;
+
+    /**
+     * @var int
+     */
+    private $height;
+
+    /**
+     * @var int
+     */
+    private $width;
+
+    /**
+     * @var array
+     */
+    private $carousel = [];
+
+    /**
+     * @var string
+     */
+    private $caption;
+
+    /**
+     * @var mixed
+     */
+    private $location;
+
+    /**
+     * @var \DateTime
+     */
+    private $date;
+
+    /**
+     * @var array
+     */
+    private $hashtags = [];
+
+    /**
+     * @var array
+     */
+    private $userTags = [];
+
+    /**
+     * @var User
+     */
+    private $user;
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getShortCode(): string
+    {
+        return $this->shortCode;
+    }
+
+    /**
+     * @param string $shortCode
+     */
+    public function setShortCode(string $shortCode): void
+    {
+        $this->shortCode = $shortCode;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLink(): string
+    {
+        return $this->link;
+    }
+
+    /**
+     * @param string $link
+     */
+    public function setLink(string $link): void
+    {
+        $this->link = $link;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLikes(): int
+    {
+        return $this->likes;
+    }
+
+    /**
+     * @param int $likes
+     */
+    public function setLikes(int $likes): void
+    {
+        $this->likes = $likes;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getIsLiked(): bool
+    {
+        return $this->isLiked;
+    }
+
+    /**
+     * @param bool $isLiked
+     */
+    public function setIsLiked(bool $isLiked): void
+    {
+        $this->isLiked = $isLiked;
+    }
+
+    /**
+     * @return int
+     */
+    public function getComments(): int
+    {
+        return $this->comments;
+    }
+
+    /**
+     * @param int $comments
+     */
+    public function setComments(int $comments): void
+    {
+        $this->comments = $comments;
+    }
+
+    /**
+     * @return int
+     */
+    public function getHeight(): int
+    {
+        return $this->height;
+    }
+
+    /**
+     * @param int $height
+     */
+    public function setHeight(int $height): void
+    {
+        $this->height = $height;
+    }
+
+    /**
+     * @return int
+     */
+    public function getWidth(): int
+    {
+        return $this->width;
+    }
+
+    /**
+     * @param int $width
+     */
+    public function setWidth(int $width): void
+    {
+        $this->width = $width;
+    }
+
+    /**
+     * @return array
+     */
+    public function getCarousel(): array
+    {
+        return $this->carousel;
+    }
+
+    /**
+     * @param array $carousel
+     */
+    public function setCarousel(array $carousel): void
+    {
+        $this->carousel = $carousel;
+    }
+
+    /**
+     * @param string $caption
+     */
+    public function setCaption(?string $caption): void
+    {
+        $this->caption = $caption;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCaption(): ?string
+    {
+        return $this->caption;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    /**
+     * @param \StdClass $location
+     */
+    public function setLocation(\StdClass $location): void
+    {
+        $this->location = $location;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDate(): \DateTime
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param \DateTime $date
+     */
+    public function setDate(\DateTime $date): void
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return array
+     */
+    public function getHashtags(): array
+    {
+        return InstagramHelper::buildHashtags($this->caption);
+    }
+
+    /**
+     * @param array $hashtags
+     */
+    public function setHashtags(array $hashtags): void
+    {
+        $this->hashtags = $hashtags;
+    }
+
+    /**
+     * @return array
+     */
+    public function getUserTags(): array
+    {
+        return $this->userTags;
+    }
+
+    /**
+     * @param array $userTags
+     */
+    public function setUserTags(array $userTags): void
+    {
+        $this->userTags = $userTags;
+    }
+
+    /**
+     * @param User $user
+     */
+    public function setUser(User $user): void
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return User
+     */
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+}
