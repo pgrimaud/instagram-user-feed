fc28efef68af9de232b9e21e6a6d5a19ce397f5c
Tim Bond <cookieguru@gmail.com>
2023-12-22 20:26:52 -0800

    Mark IMAP client test as skipped when IMAP extension is not loaded
    This prevents having the test suite fail on platforms when the extension
    is not loaded.  The test is marked as skipped so the developer knows
    that the full test suite was not run

--- tests/Auth/Checkpoint/ImapClientTest.php
+++ tests/Auth/Checkpoint/ImapClientTest.php
@@ -9,6 +9,9 @@ class ImapClientTest extends TestCase
 {
     public function testSetUpCredentials()
     {
+		if(!extension_loaded('imap')) {
+			$this->markTestSkipped('IMAP extension not loaded');
+		}
         $credentials = new ImapClient('imap.google.com', 'login', 'password');
         $this->assertSame('imap.google.com', $credentials->getServer());
         $this->assertSame('login', $credentials->getLogin());
